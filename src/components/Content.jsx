0bd8a2c80c827e245c3bb918e9eb532c59e8c989
Onos <onosejoor14@gmail.com>
2024-10-24 01:04:02 +0100

    update ip code (add see-more/less button)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,10 @@
-export default function Content({ head, content }) {
+export default function Content({ head, content,button, show,action }) {
   return (
     <>
       <div className="content-container">
         <h5 className="ipHeader">{head || "IP Adress"}</h5>
 
-        <h3 className="content">{content || <> --</>}</h3>
+        <h3 className="content">{content || <> --</>}{show ? <span> <button onClick={action} className="seeBtn">{!button ? " see-less" : " see-more"}</button></span>: null}</h3>
       </div>
     </>
   );
--- src/components/IP-Content.jsx
+++ src/components/IP-Content.jsx
@@ -1,14 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Content from "./Content";
 import { IpContext } from "./Context";
 
 const IpContent = () => {
   const { address } = useContext(IpContext);
   const { ip, timezone, location, isp } = address;
+  const [see, setSee] = useState(true)
   return (
     <>
       <div className="ip-content-container">
-        <Content content={ip} />
+        <Content content={see ? ip.slice(0,4) + "..." : ip} button={see} show={true} action={()=>setSee(prev => !prev)} />
         <Content head={"location"} content={location} />
         <Content head={"timezone"} content={"UTC "+timezone} />
         <Content head={"isp"} content={isp} />
